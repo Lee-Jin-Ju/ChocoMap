aac59a61fe58795828e8346821b5468b26449c1a
hio88 <hio88@121.131.73.172>
2023-03-18 13:52:56 +0900

    init

---
+++ chocomap/src/main/java/com/choco/chocomap/ChocomapApplication.java
@@ -0,0 +1,13 @@
+package com.choco.chocomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocomapApplication.class, args);
+	}
+
+}
---
+++ chocomap/src/test/java/com/choco/chocomap/ChocomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choco.chocomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
